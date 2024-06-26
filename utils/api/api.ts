94aacc2c62da26a943ab2cb933b1ec810f972a83
Aleksei Kosumov <AKosumovJob@gmail.com>
2024-06-26 15:08:28 +0300

    first commit

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios'
 
 export const api = axios.create({
-	baseURL: `api/v1/outlay-rows/entity/129523/row`,
+	baseURL: `/api/v1/outlay-rows/entity/129523/row`,
 	withCredentials: false,
 })
