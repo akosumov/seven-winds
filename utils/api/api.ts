8a45547f34827b21b8479388f79ea344e5318c9a
Aleksei Kosumov <AKosumovJob@gmail.com>
2024-07-17 08:38:01 +0300

    set api settings

--- utils/api/api.ts
+++ utils/api/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const api = axios.create({
-  baseURL: `http://185.244.172.108:8081/v1/outlay-rows/entity/129523/row`,
+  baseURL: `/api/v1/outlay-rows/entity/129523/row`,
   withCredentials: false,
 });
 
